19b6d1262b4bc40320f210a1729cb18a3514df8f
kosy <allisonkosy@gmail.com>
2021-01-08 09:21:39 +0100

    update licensing

--- src/emu8086aboutwin.c
+++ src/emu8086aboutwin.c
@@ -1,3 +1,22 @@
+/* gtkemu8086
+ * Copyright 2020 KRC
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * emu8086aboutwin.c
+ * About window class
+ */
+
+
+
+
 #include <stdio.h>
 #include <gtk/gtk.h>
 #include <emu8086app.h>
--- src/emu_8086_app_runner.c
+++ src/emu_8086_app_runner.c
@@ -1,3 +1,23 @@
+/* gtkemu8086
+ * Copyright 2020 KRC
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * emu_8086_app_runner.c
+ * Runner class
+ */
+
+
+
+
+
 
 #include <assembler.h>
 #include <emu8086.h>
